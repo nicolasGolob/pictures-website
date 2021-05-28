6b6ca86b04f1fd9cb850181b5083dbb4f56b10fa
Nicolas Golob <nicolasgolob@gmail.com>
2021-05-28 08:04:10 +0200

    navigation adjustment

--- Js/app.js
+++ Js/app.js
@@ -3,8 +3,11 @@ window.addEventListener('scroll',() => {
     if (window.scrollY <= 30) { 
         navbarChanging.style.backgroundColor = 'transparent'; 
     } else {
-        navbarChanging.style.backgroundColor = 'rgba(5,5,5, 0.9)';
+        navbarChanging.style.backgroundColor = '#a0a0a0';
     }
 });
+
+
+
 
 
