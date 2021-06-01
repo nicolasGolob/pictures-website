a6600d815fb936d0969da5d07756b812239e2366
Nicolas Golob <nicolasgolob@gmail.com>
2021-06-01 18:20:59 +0200

    creation of a scroll effect on the features section

--- Js/app.js
+++ Js/app.js
@@ -7,12 +7,34 @@ window.addEventListener('scroll',() => {
     }
 });
 
-/* Scroll Effect */
-gsap.to('.title-p1', {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.title-p1', start : "top 70%", end:"bottom 50%", toggleActions : "play", }});
-gsap.to('.stories-picture', { duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.stories-picture', start : "top 80%",end:"bottom 50%", toggleActions : "play",  }});
-gsap.to('.text-p1', {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.text-p1', start : "top 60%", end:"bottom 40%", toggleActions : "play",  }});
-gsap.to('.text-p2', { duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.text-p2', start : "top 50%", end:"bottom 40%", toggleActions : "play",  }});
-gsap.to('.stories-picture-2', {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.stories-picture-2', start : "top 65%", end:"bottom 40%", toggleActions : "play",   }});
+/***************** Scroll Effect ******************/
+
+/* Stories Part */
+const titleP1 = document.querySelector('.title-p1');
+const storiesPicture = document.querySelector('.stories-picture');
+const textP1 = document.querySelector('.text-p1');
+const textP2 = document.querySelector('.text-p2');
+const storiesPicture2 = document.querySelector('.stories-picture-2');
+const titlep1 = document.querySelector('.title-p1');
+
+gsap.to(titleP1, {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.title-p1', start : "top 70%", end:"bottom 50%", toggleActions : "play" }});
+gsap.to(storiesPicture, { duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.stories-picture', start : "top 80%",end:"bottom 50%", toggleActions : "play" }});
+gsap.to(textP1, {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.text-p1', start : "top 60%", end:"bottom 40%", toggleActions : "play"  }});
+gsap.to(textP2, { duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.text-p2', start : "top 50%", end:"bottom 40%", toggleActions : "play"  }});
+gsap.to(storiesPicture2, {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.stories-picture-2', start : "top 65%", end:"bottom 40%", toggleActions : "play"  }});
+
+/* Features Part -> second way to do it */
+const galleries = gsap.utils.toArray('.galery');
+galleries.forEach((galery) => {
+  gsap.to(galery, { duration:1, y:0, autoAlpha: 1,
+    scrollTrigger: {
+        trigger: galery,
+        start: "top 80%",
+        end: "bottom 40%",
+        toggleActions : "play",
+        scrub: true    }
+    });
+})
 
 
 
