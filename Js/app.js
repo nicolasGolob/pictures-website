7bcbc8aa547ef5c55c12a33c8c3a45550f62932c
Nicolas Golob <nicolasgolob@gmail.com>
2021-06-01 14:24:37 +0200

    scroll effects stories part

--- Js/app.js
+++ Js/app.js
@@ -7,8 +7,12 @@ window.addEventListener('scroll',() => {
     }
 });
 
-
-
+/* Scroll Effect */
+gsap.to('.title-p1', {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.title-p1', start : "top 70%", end:"bottom 50%", toggleActions : "play", }});
+gsap.to('.stories-picture', { duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.stories-picture', start : "top 80%",end:"bottom 50%", toggleActions : "play",  }});
+gsap.to('.text-p1', {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.text-p1', start : "top 60%", end:"bottom 40%", toggleActions : "play",  }});
+gsap.to('.text-p2', { duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.text-p2', start : "top 50%", end:"bottom 40%", toggleActions : "play",  }});
+gsap.to('.stories-picture-2', {duration:1, y:0, autoAlpha:1, scrollTrigger: { trigger:'.stories-picture-2', start : "top 65%", end:"bottom 40%", toggleActions : "play",   }});
 
 
 
